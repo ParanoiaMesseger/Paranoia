e5a20bbaa3cbf7fbb603f8015f1e45de192257ec
paranoia-dev <dev@paranoia.run>
2026-05-01 14:47:09 +0300

    - Переименовывает server build jobs в build:server:amd64/arm64/armhf   и обновляет needs: в release jobs - Добавляет hidden job .rust_setup, test-jobs переходят на extends   вместо дублированного before_script - build:client:linux: aqtinstall Qt, нативная сборка appParanoiaUiClient - build:client:android: Android NDK r27 + Qt android_arm64_v8a,   кросс-компиляция через ParanoiaLib.cmake PARANOIA_CARGO_TARGET - build:client:windows / build:client:macos: when:manual, требуют   раннер с тегом [windows]/[macos] - ParanoiaLib.cmake: поддержка PARANOIA_CARGO_TARGET для кросс-компиляции;   для Android передаёт NDK linker/CC через cmake -E env

--- ParanoiaUiClient/deps/ParanoiaLib.cmake
+++ ParanoiaUiClient/deps/ParanoiaLib.cmake
@@ -2,33 +2,57 @@
 function(setup_paranoia_lib TARGET)
     set(RUST_LIB_DIR "${CMAKE_SOURCE_DIR}/../ParanoiaLibrary")
 
-    # Собираем Rust-библиотеку
-    add_custom_target(paranoia_lib_build
-        COMMAND cargo build --release
-        WORKING_DIRECTORY "${RUST_LIB_DIR}"
-        COMMENT "Building paranoia_lib (Rust)"
-    )
-
-    # Определяем имя выходного файла под платформу
-    if(WIN32)
-        set(PARANOIA_LIB_FILE "${RUST_LIB_DIR}/target/release/paranoia_lib.lib")
+    # Кросс-компиляция: -DPARANOIA_CARGO_TARGET=aarch64-linux-android и т.п.
+    if(DEFINED PARANOIA_CARGO_TARGET AND NOT "${PARANOIA_CARGO_TARGET}" STREQUAL "")
+        set(_cargo_args --release --target "${PARANOIA_CARGO_TARGET}")
+        set(_lib_subdir "${PARANOIA_CARGO_TARGET}/release")
     else()
-        set(PARANOIA_LIB_FILE "${RUST_LIB_DIR}/target/release/libparanoia_lib.a")
+        set(_cargo_args --release)
+        set(_lib_subdir "release")
+    endif()
+
+    if(WIN32)
+        set(PARANOIA_LIB_FILE "${RUST_LIB_DIR}/target/${_lib_subdir}/paranoia_lib.lib")
+    else()
+        set(PARANOIA_LIB_FILE "${RUST_LIB_DIR}/target/${_lib_subdir}/libparanoia_lib.a")
+    endif()
+
+    # Для Android передаём NDK-линкер и C-компилятор через окружение cargo/cc
+    if(ANDROID AND DEFINED PARANOIA_CARGO_TARGET)
+        set(_ndk_bin "${ANDROID_TOOLCHAIN_ROOT}/bin")
+        string(TOUPPER "${PARANOIA_CARGO_TARGET}" _upper)
+        string(REPLACE "-" "_" _upper "${_upper}")
+        string(TOLOWER "${PARANOIA_CARGO_TARGET}" _lower)
+        string(REPLACE "-" "_" _lower "${_lower}")
+        set(_clang "${_ndk_bin}/${PARANOIA_CARGO_TARGET}${CMAKE_SYSTEM_VERSION}-clang")
+        add_custom_target(paranoia_lib_build
+            COMMAND ${CMAKE_COMMAND} -E env
+                "CARGO_TARGET_${_upper}_LINKER=${_clang}"
+                "CC_${_lower}=${_clang}"
+                "AR_${_lower}=${_ndk_bin}/llvm-ar"
+                "PATH=$ENV{PATH}:${_ndk_bin}"
+                cargo build ${_cargo_args}
+            WORKING_DIRECTORY "${RUST_LIB_DIR}"
+            COMMENT "Building paranoia_lib (Rust) → ${PARANOIA_CARGO_TARGET}"
+        )
+    else()
+        add_custom_target(paranoia_lib_build
+            COMMAND cargo build ${_cargo_args}
+            WORKING_DIRECTORY "${RUST_LIB_DIR}"
+            COMMENT "Building paranoia_lib (Rust)"
+        )
     endif()
 
     add_library(paranoia_lib STATIC IMPORTED GLOBAL)
-    set_target_properties(paranoia_lib PROPERTIES
-        IMPORTED_LOCATION "${PARANOIA_LIB_FILE}"
-    )
+    set_target_properties(paranoia_lib PROPERTIES IMPORTED_LOCATION "${PARANOIA_LIB_FILE}")
     add_dependencies(paranoia_lib paranoia_lib_build)
 
-    # Подключаем include-папку с заголовком
     target_include_directories(${TARGET} PRIVATE "${RUST_LIB_DIR}/include")
-
-    # Линкуем библиотеку + системные зависимости
     target_link_libraries(${TARGET} PRIVATE paranoia_lib)
 
-    if(UNIX AND NOT APPLE)
+    if(ANDROID)
+        target_link_libraries(${TARGET} PRIVATE log)
+    elseif(UNIX AND NOT APPLE)
         target_link_libraries(${TARGET} PRIVATE pthread dl)
     elseif(APPLE)
         target_link_libraries(${TARGET} PRIVATE
