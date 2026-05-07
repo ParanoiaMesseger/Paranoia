9f4f6cdc41262acbbced7c3d1876e83280d7ce89
paranoia-dev <dev@paranoia.run>
2026-05-07 17:52:37 +0300

    fix key name in install script

--- ParanoiaUiClient/resources/install_scripts/ConfigureNginx.sh
+++ ParanoiaUiClient/resources/install_scripts/ConfigureNginx.sh
@@ -24,7 +24,7 @@ server {
     server_name {DOMAIN};
 
     ssl_certificate     /etc/letsencrypt/live/{DOMAIN}/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/{DOMAIN}/private_key.pem;
+    ssl_certificate_key /etc/letsencrypt/live/{DOMAIN}/privkey.pem;
 
     client_max_body_size 1024M;
 
